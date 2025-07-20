083eee3edac7321ba00a3438cbd13ea647c2df8d
Nowshath06 <nows0629@gmail.com>
2025-07-20 22:22:45 +0530

    Delete KioskPro/qomander-master directory

--- KioskPro/qomander-master/src/app/modules/shared/services/meal-list/meal-list.model.ts
+++
@@ -1,8 +0,0 @@
-import { CategoriesE } from '../meal-categories/meal-categories.model';
-
-export type MealListT = {
-  name: string;
-  categoryKey: keyof typeof CategoriesE;
-  picture: string;
-  price: number;
-};
